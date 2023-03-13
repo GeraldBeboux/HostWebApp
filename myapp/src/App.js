3a4233d82da0db51279aa1364ed7b508d6e4d6fd
BEBOUX Geraldine DGAC/DTI <geraldine.beboux@aviation-civile.gouv.fr>
2023-03-13 17:43:50 +0100

    modifications

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         
           <p> This is a test to host App on web page</p>
         </p>
         <a
